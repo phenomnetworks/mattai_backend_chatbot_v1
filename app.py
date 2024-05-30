908378c0da8fcd1d5de8f58dba047b029ec70ae1
phenomnetworks <91694599+phenomnetworks@users.noreply.github.com>
2024-05-31 00:06:57 +0200

    Update app.py

--- app.py
+++ app.py
@@ -2,6 +2,9 @@ from flask import Flask, render_template, request, jsonify
 from flask_cors import CORS
 from chat import get_response
 
+import nltk
+nltk.download('punkt')
+
 app = Flask(__name__)
 CORS(app)
 
